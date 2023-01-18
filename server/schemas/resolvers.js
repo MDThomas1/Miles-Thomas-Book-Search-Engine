e858aebfe921ee400c8013d96baa897bc8d31f5a
Miles Thomas <milesman2013@gmail.com>
2023-01-18 17:56:26 +1100

    resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,62 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 const { Book, User } = require('../models')
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        }
+    },
+    Mutation: {
+        addProfile: async (parent, { name, email, password }) => {
+            const newUser = await User.create({ name, email, password });
+            const token = signToken(newUser);
+      
+            return { token, profile };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+                throw new AuthenticationError('No profile with this email found!');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password!');
+            }
+      
+            const token = signToken(profile);
+            return { token, profile };
+        },
+        addBook: async (parent, { profileId, bookId }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: profileId },
+                    {
+                        $addToSet: { savedBooks: bookId },
+                    },
+                    {
+                        new: true
+                    }
+                );
+            }
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: bookId } },
+                    { new: true }
+                );
+            }
+        }
+    }
+}
+
+module.exports = resolvers
